0fc0745e1e8d850590b2f2d5dd9f9abd7f0c6a1e
Artem Synytsin <sh1nodartem@gmail.com>
2023-11-13 18:06:00 +0200

    feature/main-page-lists: add lists to main page

---
+++ src/pages/Home/Lists/Lists.tsx
@@ -0,0 +1,56 @@
+import { type FC, useCallback } from "react";
+import { Link } from "react-router-dom";
+import ListIcon from "@mui/icons-material/List";
+import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
+
+import type { ShoppingList } from "@/shared/types";
+
+import styles from "./styles.module.scss";
+
+interface Props {
+  shoppingLists: ShoppingList[];
+  handleUnpinShoppingList: (listId: string) => Promise<void>;
+}
+
+const Lists: FC<Props> = (props) => {
+  const getCloseIconClickHandler = useCallback(
+    (listId: string) => (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+      event.preventDefault();
+
+      void props.handleUnpinShoppingList(listId);
+    },
+    [props.handleUnpinShoppingList]
+  );
+
+  return (
+    <>
+      <h2 className={styles.title}>Previous Lists</h2>
+      <ul className={styles.list}>
+        {props.shoppingLists.map((shoppingList) => (
+          <li key={shoppingList.id} className={styles.item}>
+            <Link to={`/shopping-list/${shoppingList.id}`} className={styles.link}>
+              <Button
+                variant="contained"
+                size="small"
+                startIcon={<ListIcon />}
+                endIcon={
+                  <CloseIcon
+                    className={styles.closeIcon}
+                    onClick={getCloseIconClickHandler(shoppingList.id)}
+                  />
+                }
+              >
+                <h3 className={styles.itemTitle}>{shoppingList.title}</h3>
+              </Button>
+              {/* <span className={styles.iconWrapper}><ListIcon /></span> */}
+              {/* <h3 className={styles.itemTitle}>{shoppingList.title}</h3> */}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Lists;
