f40b0a57f31d0d1145a1c8f11b53a1e71998e223
Artem Synytsin <sh1nodartem@gmail.com>
2023-11-25 13:34:44 +0200

    feature/add-vite-pwa: add vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 import path from "path";
 
 export default defineConfig({
   server: {
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+       registerType: 'autoUpdate',
+       
+       })
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
