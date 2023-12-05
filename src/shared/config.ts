503c4568d2bcf4e09f1ee79358c1a8b35e601927
Artem Synytsin <sh1nodartem@gmail.com>
2023-12-05 20:57:34 +0200

    feature/add-vite-pwa: add new api

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,2 +1,1 @@
-export const API_URL =
-  import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_PROD;
+export const API_URL = "/api";
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
   server: {
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        headers: { origin: "http://localhost:8080" },
+      },
+    },
   },
   plugins: [
     react(),
     VitePWA({
-       registerType: 'autoUpdate',
-       
-       })
+      registerType: "autoUpdate",
+    }),
   ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
