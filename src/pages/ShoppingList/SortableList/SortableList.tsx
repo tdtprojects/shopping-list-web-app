35b2062c650a45a0cd2db95c9df6c991bd9c33db
Artem Synytsin <sh1nodartem@gmail.com>
2023-12-29 18:10:53 +0200

    feature/add-remove-items-on-keydown: add items on enter key

--- src/pages/ShoppingList/SortableList/SortableList.tsx
+++ src/pages/ShoppingList/SortableList/SortableList.tsx
@@ -20,11 +20,9 @@ interface Props {
 const SortableList: FC<Props> = (props) => {
   const [shoppingListItems, setShoppingListItems] = useState<ShoppingListItem[]>(props.itemList);
   const prevShoppingListItemsRef = useRef<ShoppingListItem[]>([]);
-  const lastItemRef = useRef<HTMLTextAreaElement>(null);
+  const itemsRefs = useRef<HTMLTextAreaElement[]>([]);
   const isFirstRender = useRef(true);
-  const { shoppingListId, title } = props;
-
-  const { updateShoppingListItems } = props;
+  const { shoppingListId, title, updateShoppingListItems } = props;
   const column: ColumnType = useMemo(
     () => ({
       id: shoppingListId,
@@ -34,18 +32,20 @@ const SortableList: FC<Props> = (props) => {
   );
 
   useEffect(() => {
-    const hasNewShoppingItemAdded = isFirstRender.current ? false : (
-      shoppingListItems.length > Number(prevShoppingListItemsRef?.current?.length)
+    const newItem = isFirstRender.current ? null : (
+      shoppingListItems.find((item) => !prevShoppingListItemsRef.current.some(({ id }) => id === item.id))
     );
 
-    if (hasNewShoppingItemAdded && !isNil(lastItemRef.current)) {
-      const { length } = lastItemRef.current.value;
+    if (!isNil(newItem)) {
+      const newItemRef = itemsRefs.current[newItem.order - 1];
+      const { length } = newItemRef.value;
 
-      lastItemRef.current.focus();
-      lastItemRef.current.setSelectionRange(length, length);
+      newItemRef.focus();
+      newItemRef.setSelectionRange(length, length);
     }
 
     prevShoppingListItemsRef.current = shoppingListItems;
+    itemsRefs.current = itemsRefs.current.slice(0, shoppingListItems.length);
   }, [shoppingListItems]);
 
   useEffect(() => {
@@ -148,6 +148,32 @@ const SortableList: FC<Props> = (props) => {
   };
 
   const handleItemKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, itemId: string): void => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+
+      const item = shoppingListItems.find((item) => item.id === itemId) as ShoppingListItem;
+      const newItem = {
+        checked: false,
+        text: "",
+        id: v4(),
+        order: item.order + 1,
+      };
+      const updatedShoppingListItems = shoppingListItems.reduce((result: ShoppingListItem[], currentValue) => {
+        if (currentValue.order === item.order) {
+          return [...result, currentValue, newItem];
+        }
+
+        if (currentValue.order < item.order) {
+          return [...result, currentValue];
+        } else {
+          return [...result, { ...currentValue, order: currentValue.order + 1 }];
+        }
+      }, []);
+
+      setShoppingListItems(updatedShoppingListItems);
+      updateShoppingList(updatedShoppingListItems);
+    }
+
     if (event.metaKey && event.key === "z") {
       const isItemNew = validateV4(itemId);
       const target = event.target as HTMLTextAreaElement;
@@ -181,7 +207,7 @@ const SortableList: FC<Props> = (props) => {
         handleColumnTitleBlur={handleColumnTitleBlur}
         deleteShoppingList={props.deleteShoppingList}
         handleItemKeyDown={handleItemKeyDown}
-        lastItemRef={lastItemRef}
+        itemsRefs={itemsRefs}
       />
     </DragDropContext>
   );
